8fba1e2281df3b4e10ade8bd4ce301e7422595e3
Chris Dent <chris.dent@gmail.com>
2011-06-08 16:14:35 +0100

    initial commit of twp.remotebag

---
+++ setup.py
@@ -0,0 +1,29 @@
+AUTHOR = 'Chris Dent'
+AUTHOR_EMAIL = 'cdent@peermore.com'
+NAME = 'tiddlywebplugins.remotebag'
+DESCRIPTION = 'Access remote content as a bag'
+VERSION = '0.1'
+
+
+import os
+
+from setuptools import setup, find_packages
+
+
+setup(
+    namespace_packages = ['tiddlywebplugins'],
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = open(os.path.join(os.path.dirname(__file__),
+        'README')).read(),
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = 'http://pypi.python.org/pypi/%s' % NAME,
+    platforms = 'Posix; MacOS X; Windows',
+    packages = find_packages(exclude=['test']),
+    install_requires = ['setuptools',
+        'tiddlyweb',
+        'httplib2'],
+    zip_safe = False
+    )
